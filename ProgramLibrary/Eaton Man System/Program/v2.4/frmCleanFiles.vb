3cb6ad90633618e2ce6bac75a6323773a29a52d3
priceluke <lp00579@surrey.ac.uk>
2018-11-15 20:54:41 +0000

    ez

--- ProgramLibrary/Eaton Man System/Program/v2.4/frmCleanFiles.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class frmCleanFiles
-    Private Sub btnCleanFiles_Click(sender As Object, e As EventArgs) Handles btnCleanFiles.Click
-        clean_files()
-    End Sub
-End Class
--- ProgramLibrary/Eaton Man System/Program/v3.1/frmFrequencyReports.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class frmFrequencyReports
-
-End Class
